c3a7d07e6bb749b2b36d991bbacb0c08ddd9f578
loxygenK <loxygen.k@gmail.com>
2020-07-22 14:40:26 +0900

    †追加† 思いつくかぎりのバリデーションを追加した

--- lib/discord/command/impl/CreateTweetVoteCommand.py
+++ lib/discord/command/impl/CreateTweetVoteCommand.py
@@ -1,4 +1,7 @@
+import re
+import unicodedata
 from abc import ABC
+from math import ceil
 
 import discord
 
@@ -8,15 +11,45 @@ from lib.logging.Logger import log
 
 
 class CreateTweetVoteCommand(ABCCommand, ABC):
+    SPECIAL_CHARACTER_REGEX: re.Pattern = re.compile("<[@#:].*?>")
 
     def get_command_info(self) -> CommandInfo:
         return CommandInfo(
-           identify="create",
-           args_format="(ツイートの内容)",
-           name="ツイートを作成する",
-           description="ツイートしたい内容を"
+            identify="create",
+            args_format="(ツイートの内容)",
+            name="ツイートを作成する",
+            description="ツイートしたい内容を"
         )
 
     async def parse_command(self, text: str, message: discord.Message):
         log("command-create", "ツイートの作成コマンドを受信しました。")
-        await message.channel.send("`{}`".format(text))
+
+        apparent_len = get_apparently_length(text)
+        if apparent_len > 240:
+            log("command-create", "文字列が長すぎました。({} > 240)".format(apparent_len))
+            await message.channel.send(
+                "テキストが長すぎるみたいです:thinking:\n"
+                "{}文字あって{}文字オーバーしてるので削ってみてください。"
+                .format(ceil(apparent_len / 2), ceil(apparent_len / 2 - 240))
+            )
+            return
+
+        if CreateTweetVoteCommand.SPECIAL_CHARACTER_REGEX.match(text) is not None:
+            log("command-create", "特殊な文字列が含まれています。")
+            await message.channel.send(
+                "特殊な文字列が含まれています:thinking:\n"
+                "メンションやこの鯖独自の絵文字(<:ahe:724540322322972723>とか)はツイートできません。使えたら面白いんだけどな〜"
+            )
+            return
+
+        log("command-create", "問題ありませんでした。")
+        await message.channel.send("Passed:```{}```".format(text))
+
+
+def get_apparently_length(text: str) -> int:
+    length = 0
+    for c in text:
+        width_text = unicodedata.east_asian_width(c)
+        length += 2 if width_text in ["F", "W", "A"] else 1
+
+    return length
