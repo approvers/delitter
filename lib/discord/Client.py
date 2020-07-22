fbc684ccd8587539ebbf920e48a7209f480acf2b
loxygenK <loxygen.k@gmail.com>
2020-07-22 17:58:51 +0900

    †編集† カプセル化(笑)を行った

--- lib/discord/Client.py
+++ lib/discord/Client.py
@@ -2,7 +2,7 @@ from typing import Dict, Type, List
 
 import discord
 
-from lib.discord.MainClientSeting import MainClientSetting
+from lib.discord.Setting import Setting
 from lib.discord.command.ABCCommand import ABCCommand
 from lib.logging.Logger import log
 
@@ -12,16 +12,32 @@ class MainClient(discord.Client):
     DiscordのBot。
     """
 
-    def __init__(self, setting: MainClientSetting):
+    def __init__(self, setting: Setting):
         """
         指定した設定でクライアントを初期化する。
         :param setting: Botに使用する設定。
         """
         super(MainClient, self).__init__()
-        self.setting: MainClientSetting = setting
-        self.activity_channel: discord.TextChannel = None
-        self.commands_type: List[Type[ABCCommand]] = []
-        self.commands: Dict[str, ABCCommand] = {}
+        self.__setting: Setting = setting
+        self.__activity_channel: discord.TextChannel = None
+        self.__commands_type: List[Type[ABCCommand]] = []
+        self.__commands: Dict[str, ABCCommand] = {}
+
+    @property
+    def setting(self):
+        pass
+
+    @property
+    def activity_channel(self):
+        pass
+
+    @setting.getter
+    def setting(self) -> Setting:
+        return self.__setting
+
+    @activity_channel.getter
+    def activity_channel(self) -> discord.TextChannel:
+        return self.__activity_channel
 
     def launch(self):
         """
@@ -29,46 +45,46 @@ class MainClient(discord.Client):
         Botが何らかの理由で終了するまで処理は停止する。
         """
         log("client-login", "ログイン処理を開始します。")
-        self.run(self.setting.token)
+        self.run(self.__setting.token)
 
     def add_command(self, command: Type[ABCCommand]):
-        self.commands_type.append(command)
+        self.__commands_type.append(command)
 
     async def on_ready(self):
         log("client-login", "ログインに成功しました。適切な設定が行われているか確認しています。")
-        self.activity_channel = self.get_channel(self.setting.activity_channel_id)
+        self.__activity_channel = self.get_channel(self.__setting.activity_channel_id)
 
-        if self.activity_channel is None:
+        if self.__activity_channel is None:
             raise RuntimeError("Activity channel is not found! Check your \"activity_channel_id\" value.")
 
         log("client-login", "設定に問題はありませんでした。コマンドのインスタンスを生成します…")
 
-        for command in self.commands_type:
+        for command in self.__commands_type:
             command_instance = command(self)
-            self.commands[command_instance.get_command_info().identify] = command_instance
+            self.__commands[command_instance.get_command_info().identify] = command_instance
 
         log("client-login", "問題は発生しませんでした。起動メッセージを送信します…")
-        await self.activity_channel.send("***†Delitter Ready†***")
+        await self.__activity_channel.send("***†Delitter Ready†***")
 
     async def on_message(self, message: discord.Message):
-        if message.author.bot or message.channel.id != self.setting.activity_channel_id:
+        if message.author.bot or message.channel.id != self.__setting.activity_channel_id:
             log("client-msg", "メッセージはBotからのものか、Activity Channelではないところで発言されたものでした。無視します！")
             return
 
-        if not message.content.startswith(self.setting.prefix):
-            log("client-msg", "メッセージは処理対象でしたが、Prefix「{}」がありませんでした。無視します！".format(self.setting.prefix))
+        if not message.content.startswith(self.__setting.prefix):
+            log("client-msg", "メッセージは処理対象でしたが、Prefix「{}」がありませんでした。無視します！".format(self.__setting.prefix))
             return
 
         log("client-msg", "処理対象のメッセージを受信しました:\n{}".format(message.content))
 
         cmd_header = message.content.split(" ")[0]
-        cmd_identity = cmd_header[len(self.setting.prefix):]
-        if cmd_identity not in self.commands:
+        cmd_identity = cmd_header[len(self.__setting.prefix):]
+        if cmd_identity not in self.__commands:
             log("client-msg", "コマンドが見つかりませんでした: 「{}」".format(cmd_identity))
-            await self.activity_channel.send("知らないコマンドが出てきました:thinking:")
+            await self.__activity_channel.send("知らないコマンドが出てきました:thinking:")
             return
 
-        await self.commands[cmd_identity].parse_command(
+        await self.__commands[cmd_identity].parse_command(
             message.content[len(cmd_header) + 1:],
             message
         )
