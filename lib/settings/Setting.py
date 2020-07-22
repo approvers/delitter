c3bc3c6219469cc26a620f03fd081e92653dba78
loxygenK <loxygen.k@gmail.com>
2020-07-22 22:30:27 +0900

    †追加† 判定基準周りの設定を生やした

--- lib/settings/Setting.py
+++ lib/settings/Setting.py
@@ -13,6 +13,8 @@ class Setting:
                  prefix: str,
                  suffrage_role_id: int,
                  emoji_ids: Dict[str, int],
+                 approve_total: int,
+                 approve_rate: int
                  ):
         """
         MainClientクラスで使用する設定を保持するクラス。
@@ -20,6 +22,10 @@ class Setting:
                       Noneが渡された場合はDISCORD_TOKEN環境変数からの読み出しを試みる。
         :param activity_channel_id: 動作するチャンネル。
         :param prefix: Botのプレフィックス。
+        :param suffrage_role_id: 参政権ロールのID。
+        :param emoji_ids: 投票に使用する絵文字のID。
+        :param approve_total: 決議に必要な人数。
+        :param approve_rate: 可決となるための可決率。
         """
 
         if token is None and "DISCORD_TOKEN" not in os.environ:
@@ -30,6 +36,8 @@ class Setting:
         self.prefix = prefix
         self.suffrage_role_id = suffrage_role_id
         self.emoji_ids = emoji_ids
+        self.approve_total = approve_total
+        self.approve_rate = approve_rate
 
     @classmethod
     def load_from_json(cls, file: TextIO):
@@ -46,5 +54,7 @@ class Setting:
             raw_json["activity_channel_id"],
             raw_json["prefix"],
             raw_json["suffrage_role_id"],
-            raw_json["emoji_ids"]
+            raw_json["emoji_ids"],
+            raw_json["judge_standard"]["total"],
+            raw_json["judge_standard"]["rate"]
         )
