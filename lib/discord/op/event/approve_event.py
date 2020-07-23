f75615dc2e24f3b31ae4a61578daf503cb540b2a
loxygenK <loxygen.k@gmail.com>
2020-07-23 18:39:24 +0900

    †削除† 余剰なメッセージを消去した

--- lib/discord/op/event/approve_event.py
+++ lib/discord/op/event/approve_event.py
@@ -20,6 +20,6 @@ async def on_approved(message: discord.Message, vote_record: TweetsVoteRecord):
 
     new_embed = message.embeds[0]
     new_embed.title = "†可決†"
-    await message.edit(content="この投票は可決されました！もう受け付けていません。", embed=new_embed)
+    await message.edit(content="この投票は可決されました！", embed=new_embed)
 
     vote_record.delete(message.id)
