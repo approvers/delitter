cb6363759273fed2ff384dbcd1539b7581a33d1b
loxygenK <loxygen.k@gmail.com>
2020-07-25 18:09:58 +0900

    †追加† 可決したツイートがツイートできるようになった

--- main.py
+++ main.py
@@ -9,9 +9,11 @@ from lib.discord.op.command.command_register import CommandRegister
 from lib.discord.op.command.impl.create_vote_command import CreateVoteCommand
 from lib.discord.op.command.impl.delete_vote_command import DeleteVoteCommand
 from lib.discord.op.event.approve_discord_handler import ApproveDiscordHandler
+from lib.discord.op.event.approve_twitter_handler import ApproveTwitterHandler
 from lib.discord.op.event.reaction_event import ReactionEvent
 from lib.settings.discord_setting import DiscordSetting, create_discord_setting_from_json
 from lib.settings.twitter_setting import TwitterSetting, create_twitter_setting_from_json
+from lib.twitter.tweeter import Tweeter
 
 if __name__ == '__main__':
     with open("settings/discord.json", mode="r") as f:
@@ -28,6 +30,7 @@ if __name__ == '__main__':
 
     reaction_event_handler = ReactionEvent(discord_setting, votes_record)
     approve_event_handlers = [
+        ApproveTwitterHandler(Tweeter(twitter_setting)),
         ApproveDiscordHandler()
     ]
 
