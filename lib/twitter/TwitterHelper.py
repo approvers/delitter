cde435c070ceb8d643b9ad92655df81820a817b5
loxygenK <loxygen.k@gmail.com>
2020-07-25 17:34:15 +0900

    †追加† ツイートできるようになった！

--- lib/twitter/TwitterHelper.py
+++ lib/twitter/TwitterHelper.py
@@ -1,7 +1,21 @@
-from lib.settings.twitter_settings import TwitterSetting
+from twitter import *
+
+from lib.logging.logger import log
+from lib.settings.twitter_setting import TwitterSetting
 
 
 class TwitterHelper:
 
     def __init__(self, setting: TwitterSetting):
-        pass
+        self.twitter = Twitter(
+            auth=OAuth(
+                setting.access_token,
+                setting.access_secret_token,
+                setting.consumer_key,
+                setting.consumer_secret_key
+            )
+        )
+
+    def tweet(self, content: str):
+        log("twitter-helper", "以下の内容でツイートが行われました: {}".format(content))
+        self.twitter.statuses.update(status=content)
